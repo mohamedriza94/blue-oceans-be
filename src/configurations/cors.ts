4c5fc729b9fbb5de76e712e30b5d9411e31b4e08
Mohamed Riza <mohamedriza94@gmail.com>
2025-01-23 16:26:00 +0530

    update

--- src/configurations/cors.ts
+++ src/configurations/cors.ts
@@ -2,7 +2,7 @@ import cors from "cors";
 import { envData } from "../constants/env-data";
 
 const corsOptions = {
-  origin: [envData.frontendBusinessDashboardURI || "http://localhost:3000"],
+  origin: [envData.frontendBusinessDashboardURI || "http://localhost:3000", "http://localhost:3000"],
   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
   credentials: true, // Allow credentials (e.g., cookies)
   optionsSuccessStatus: 200, // For older browsers
